16ecffbef34344a253cc9054987d85ccf71585ee
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-09 23:43:34 +0000

    key value to urldatabase

--- express_server.js
+++ express_server.js
@@ -1,9 +1,7 @@
 function generateRandomString() {
   var newURL = Math.random().toString(36).substring(7);
-  console.log(newURL);
+  return newURL;
 }
-
-generateRandomString();
 
 var express = require("express");
 var app = express();
@@ -15,8 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  // "b2xVn2": "http://www.lighthouselabs.ca",
+  // "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
@@ -45,7 +43,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  var short = req.params.shortURL;
+  let short = req.params.shortURL;
   let templateVars = { shortURL: short,
                        longURL: urlDatabase[short] };
   res.render("urls_show", templateVars);
@@ -53,5 +51,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let urlKey = generateRandomString();
+  urlDatabase[urlKey] = req.body.longURL;
+  res.send(`/urls/${urlKey}`);         // Respond with 'Ok' (we will replace this)
 });
