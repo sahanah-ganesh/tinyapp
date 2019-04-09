2226e9f4b5cf29e97a8842a1ca571ccf50c99bda
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-09 23:55:32 +0000

    added redirect to tinyurl pairing with @juliamoses

--- express_server.js
+++ express_server.js
@@ -50,8 +50,13 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log(req.body);
   let urlKey = generateRandomString();
   urlDatabase[urlKey] = req.body.longURL;
-  res.send(`/urls/${urlKey}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${urlKey}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = req.body.longURL;
+  res.redirect(longURL);
 });
