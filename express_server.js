ae7390f35b39b1128bee349c7bede0fdf4279b83
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-09 22:06:27 +0000

    completed form creation

--- express_server.js
+++ express_server.js
@@ -1,6 +1,13 @@
+function generateRandomString() {
+
+}
+
 var express = require("express");
 var app = express();
 var PORT = 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,7 +15,6 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,9 +37,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   var short = req.params.shortURL;
   let templateVars = { shortURL: short,
                        longURL: urlDatabase[short] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
