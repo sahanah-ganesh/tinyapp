34fa71688a736ac09195c2a70c5666d4ae8a116a
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-09 17:26:20 +0000

    list long and short urls and link to new

--- express_server.js
+++ express_server.js
@@ -4,14 +4,11 @@ var PORT = 8080;
 
 app.set("view engine", "ejs");
 
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
-
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,4 +24,9 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
